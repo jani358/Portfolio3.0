eac50ac4cec54ea223afbf4964cd48d3be37a52b
Janindu Gayanga <janindugayanga10@gmail.com>
2024-02-17 19:41:09 +0530

    made some changes and updated few components

---
+++ src/themes/default.js
@@ -0,0 +1,25 @@
+export default {
+  fonts: {
+    title: "Montserrat, sans-serif",
+    main: "Lato, sans-serif",
+  },
+
+  colors: {
+    primary1: "#FF0000",
+    background1: "#FFFFFF",
+    button: "#00FF00",
+    background2: "#000080",
+    text: "#808080",
+    text1: "#000000",
+    text2: "#800080",
+    text3: "#FFFF00",
+    footerBackground: "#808000",
+  },
+  // Breakpoints
+  breakpoints: {
+    sm: "screen and (max-width: 640px)",
+    md: "screen and (max-width: 768px)",
+    lg: "screen and (max-width: 1024px)",
+    xl: "screen and (max-width: 1280px)",
+  },
+};
---
+++ src/utils/Themes.js
@@ -0,0 +1,22 @@
+export const darkTheme = {
+  bg: "#222222",
+  bgLight: "#333333",
+  primary: "#2E8B57",            
+  text_primary: "#F2F3F4",
+  text_secondary: "#b1b2b3",
+  card: "#171721",
+  card_light: '#1E1E1E', 
+  button: "#FFD700",             
+  white: "#FFFFFF",
+  black: "#000000"
+}
+
+export const lightTheme = {
+  bg: "#E0E0FF", 
+  bgLight: "#f0f0f0",
+  primary: "#FFFFF0",           
+  text_primary: "#111111",
+  text_secondary: "#48494a",
+  card: "#FFFFFF",
+  button: "#5c5b5b"
+}
